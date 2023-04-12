549ace6f38085b9c39480eacb635bdcef3cca735
Ramon Wiethorn <ramonwiethorn@gmail.com>
2023-04-12 14:58:30 -0300

    Arquivos corrigidos

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-const btn = document.querySelector('#btnvalidar');
+const form = document.getElementById('formulario');
+	
 
-function maiorQue (n1,n2){
+	function maiorQue (n1,n2){
     if (n1>=n2){
-        console.log("Não Autorizado");
-    }else{
-        console.log("Autorizado");
-    }
+	alert("Não Autorizado");
+    n1 ='';
+    n2 ='';
+	}else{
+	alert("Autorizado");
+    n1 ='';
+    n2 ='';
+	}
 }
-
-
-btn = addEventListener('click', function(e){
+	
+form.addEventListener('submit',function(e){
     e.preventDefault();
+
 
     const n1 = document.querySelector('#n1');
     const n2 = document.querySelector('#n2');
     const value1 = n1.value;
     const value2 = n2.value;
 
-    console.log(value1);
-    console.log(value2);
-    maiorQue(value1,value2);
+	maiorQue(value1,value2);
 });
